1ec53a00e89e8f1a4f8630ea10ca090c05614d25
luoheng <1301089462@qq.com>
2019-09-16 15:34:48 +0800

    merge countBitmap and normalBitmap

--- bitmap.go
+++ bitmap.go
@@ -3,6 +3,7 @@ package bitmap
 import (
 	"bytes"
 	"fmt"
+	"math"
 )
 
 type bitInt uint
@@ -12,22 +13,31 @@ const (
 	bitmapSize = 4
 )
 
-// Bitmap is a bitSet
-type Bitmap struct {
+// Bitmap is the interface of bitSet
+type Bitmap interface {
+	Add(x int)
+	Has(x int)
+	Remove(x int)
+	Len() int
+	Clear()
+}
+
+// NBitmap is a normal bitSet
+type NBitmap struct {
 	len   int
 	words []bitInt
 }
 
 // New return a new bitmap
-func New() *Bitmap {
-	return &Bitmap{
+func New() *NBitmap {
+	return &NBitmap{
 		len:   0,
 		words: make([]bitInt, bitmapSize),
 	}
 }
 
 // String return formated string of bitmap
-func (b *Bitmap) String() string {
+func (b *NBitmap) String() string {
 	var buf bytes.Buffer
 	buf.WriteByte('{')
 	for i, word := range b.words {
@@ -47,12 +57,12 @@ func (b *Bitmap) String() string {
 }
 
 // Len return numbers in bitmap
-func (b *Bitmap) Len() int {
+func (b *NBitmap) Len() int {
 	return b.len
 }
 
 // Has return true if x is in the bitmap
-func (b *Bitmap) Has(x int) bool {
+func (b *NBitmap) Has(x int) bool {
 	if x < 0 {
 		return false
 	}
@@ -61,7 +71,7 @@ func (b *Bitmap) Has(x int) bool {
 }
 
 // Add add x to the bitmap
-func (b *Bitmap) Add(x int) {
+func (b *NBitmap) Add(x int) {
 	if x < 0 {
 		return
 	}
@@ -77,7 +87,7 @@ func (b *Bitmap) Add(x int) {
 }
 
 // Remove remove x in bitmap
-func (b *Bitmap) Remove(x int) {
+func (b *NBitmap) Remove(x int) {
 	if x < 0 {
 		return
 	}
@@ -92,13 +102,13 @@ func (b *Bitmap) Remove(x int) {
 }
 
 // Clear make the bitmap empty
-func (b *Bitmap) Clear() {
+func (b *NBitmap) Clear() {
 	*b = *New()
 }
 
 // Copy return a copy bitmap
-func (b *Bitmap) Copy() *Bitmap {
-	new := Bitmap{}
+func (b *NBitmap) Copy() *NBitmap {
+	new := NBitmap{}
 	new.len = b.len
 	new.words = make([]bitInt, len(b.words))
 	copy(new.words, b.words)
@@ -107,7 +117,7 @@ func (b *Bitmap) Copy() *Bitmap {
 
 // Union b = b | c
 // elements in b or c
-func (b *Bitmap) Union(c *Bitmap) {
+func (b *NBitmap) Union(c *NBitmap) {
 	length := 0
 	for i, cword := range c.words {
 		if i >= len(b.words) {
@@ -123,7 +133,7 @@ func (b *Bitmap) Union(c *Bitmap) {
 
 // Intersect b = b & c
 // elements both in b and c
-func (b *Bitmap) Intersect(c *Bitmap) {
+func (b *NBitmap) Intersect(c *NBitmap) {
 	for i, cwords := range c.words {
 		if i >= len(b.words) {
 			break
@@ -137,7 +147,7 @@ func (b *Bitmap) Intersect(c *Bitmap) {
 
 // Except b = b - c
 // elements only in b
-func (b *Bitmap) Except(c *Bitmap) {
+func (b *NBitmap) Except(c *NBitmap) {
 	for i, cwords := range c.words {
 		if i >= len(b.words) {
 			break
@@ -148,7 +158,7 @@ func (b *Bitmap) Except(c *Bitmap) {
 
 // SymExcept b = (b - c) | (c - b)
 // elements only in b or only in c
-func (b *Bitmap) SymExcept(c *Bitmap) {
+func (b *NBitmap) SymExcept(c *NBitmap) {
 	length := 0
 	for i, cwords := range c.words {
 		if i >= len(b.words) {
@@ -160,4 +170,145 @@ func (b *Bitmap) SymExcept(c *Bitmap) {
 	if length != 0 {
 		b.words = append(b.words, c.words[length:]...)
 	}
+}
+
+// CBitmap is a bitSet
+type CBitmap struct {
+	len     int
+	n       int
+	bitSize int
+	mask    int
+	numSize int
+	words   []bitInt
+}
+
+// NewC return a new bitmap
+func NewC(n int) *CBitmap {
+	if n <= 0 || n > (1<<(bitSize-1)-1) {
+		return nil
+	}
+	numSize := int(math.Log2(float64(n)))
+	cb := CBitmap{}
+	cb.len = 0
+	cb.n = n
+	cb.numSize = numSize + 1
+	cb.bitSize = bitSize / cb.numSize
+	cb.mask = (1 << bitInt(cb.numSize)) - 1
+	cb.words = make([]bitInt, bitmapSize)
+	return &cb
+}
+
+// String return formated string of bitmap
+func (cb *CBitmap) String() string {
+	var buf bytes.Buffer
+	buf.WriteByte('{')
+	for i, word := range cb.words {
+		if word != 0 {
+			for j := 0; j < cb.bitSize; j += cb.numSize {
+				if word&(bitInt(cb.mask<<bitInt(j))) != 0 {
+					if buf.Len() > len("{") {
+						buf.WriteByte(' ')
+					}
+					fmt.Fprintf(&buf, "%d", cb.bitSize*i+j/cb.numSize)
+				}
+			}
+		}
+	}
+	buf.WriteByte('}')
+	return buf.String()
+}
+
+// Len return numbers in bitmap
+func (cb *CBitmap) Len() int {
+	return cb.len
+}
+
+// Has return true if x is in the bitmap
+func (cb *CBitmap) Has(x int) bool {
+	if x < 0 {
+		return false
+	}
+	word, bit := x/cb.bitSize, bitInt(x%cb.bitSize*cb.numSize)
+	return word < len(cb.words) && cb.words[word]&(1<<bit) != 0
+}
+
+// Add add x to the bitmap
+func (cb *CBitmap) Add(x int) {
+	if x < 0 {
+		return
+	}
+	word, bit := x/cb.bitSize, bitInt(x%cb.bitSize*cb.numSize)
+	if word >= len(cb.words) {
+		cb.words = append(cb.words, make([]bitInt, word+1-len(cb.words))...)
+	}
+	numSize := bitInt(cb.mask << bit)
+	if cb.words[word]&numSize == 0 {
+		cb.len++
+	}
+	if ((cb.words[word] & numSize) >> bit) != bitInt(cb.mask) {
+		cb.words[word] += 1 << bit
+	}
+}
+
+// Remove remove x in bitmap
+func (cb *CBitmap) Remove(x int) {
+	if x < 0 {
+		return
+	}
+	word, bit := x/cb.bitSize, bitInt(x%cb.bitSize*cb.numSize)
+	if word < len(cb.words) {
+		numSize := bitInt(cb.mask << bit)
+		if cb.words[word]&numSize != 0 {
+			cb.words[word] -= 1 << bit
+			if cb.words[word]&numSize == 0 {
+				cb.len--
+			}
+		}
+	}
+}
+
+// Count return the numSize of x elements
+func (cb *CBitmap) Count(x int) int {
+	if x < 0 {
+		return 0
+	}
+	word, bit := x/cb.bitSize, bitInt(x%cb.bitSize*cb.numSize)
+	if word >= len(cb.words) {
+		return 0
+	}
+	numSize := bitInt(cb.mask << bit)
+	return int((numSize & cb.words[word]) >> bit)
+}
+
+// RemoveAll remove x in bitmap
+func (cb *CBitmap) RemoveAll(x int) {
+	if x < 0 {
+		return
+	}
+	word, bit := x/cb.bitSize, bitInt(x%cb.bitSize*cb.numSize)
+	if word < len(cb.words) {
+		numSize := bitInt(cb.mask << bit)
+		if cb.words[word]&numSize != 0 {
+			cb.len--
+			cb.words[word] &^= numSize
+		}
+	}
+}
+
+// Clear make the bitmap empty
+func (cb *CBitmap) Clear() {
+	*cb = *NewC(cb.n)
+}
+
+// Copy return a copy bitmap
+func (cb *CBitmap) Copy() *CBitmap {
+	new := CBitmap{}
+	new.len = cb.len
+	new.n = cb.n
+	new.mask = cb.mask
+	new.bitSize = cb.bitSize
+	new.numSize = cb.numSize
+	new.words = make([]bitInt, len(cb.words))
+	copy(new.words, cb.words)
+	return &new
 }
--- bitmap_test.go
+++ bitmap_test.go
@@ -140,7 +140,137 @@ func TestSets(t *testing.T) {
 
 func BenchmarkBitmap(b *testing.B) {
 	bm := bitmap.New()
-	const memory = 1000000000
+	const memory = 100000000
+	for i := 0; i < b.N; i++ {
+		bm.Add(i % memory)
+		bm.Has(i % memory)
+		bm.Remove(i % memory)
+	}
+}
+
+func TestCAdd(t *testing.T) {
+	b := bitmap.NewC(3)
+	b.Add(-1)
+	b.Add(0)
+	b.Add(1)
+	b.Add(2)
+	b.Add(10000)
+	if b.Len() != 4 && b.String() != "{0 1 2 10000}" {
+		t.Errorf("TestAdd failed. Expected {0 1 2 10000}, Got %s", b.String())
+	}
+}
+
+func TestCHas(t *testing.T) {
+	b := bitmap.NewC(3)
+	b.Add(-1)
+	b.Add(0)
+	b.Add(1)
+	b.Add(2)
+	b.Add(10000)
+	if b.Has(-1) || !b.Has(0) || !b.Has(1) || !b.Has(2) || b.Has(3) || !b.Has(10000) {
+		t.Errorf("TestHas failed.")
+	}
+}
+
+func TestCRemove(t *testing.T) {
+	b := bitmap.NewC(3)
+	b.Add(-1)
+	b.Add(0)
+	b.Add(1)
+	b.Add(2)
+	b.Add(2)
+	b.Add(4)
+	b.Remove(3)
+	if b.String() != "{0 1 2 4}" {
+		t.Errorf("TestRemove failed. Expected {0 1 2 4}, Got %s", b.String())
+	}
+	b.Remove(4)
+	if b.Has(4) {
+		t.Errorf("TestRemove failed. Expected false, Got true")
+	}
+	b.Remove(-1)
+	b.Remove(1)
+	b.Remove(2)
+	b.Remove(2)
+	b.Remove(0)
+	if b.String() != "{}" {
+		t.Errorf("TestRemove failed. Expected {}, Got %s", b.String())
+	}
+}
+
+func TestCClear(t *testing.T) {
+	b := bitmap.NewC(3)
+	b.Add(-1)
+	b.Add(0)
+	b.Add(1)
+	b.Add(2)
+	b.Add(10000)
+	b.Clear()
+	if b.Has(0) {
+		t.Errorf("TestClear failed")
+	}
+}
+
+func TestCLen(t *testing.T) {
+	b := bitmap.NewC(3)
+	b.Add(-1)
+	b.Add(0)
+	b.Add(1)
+	b.Add(2)
+	b.Add(10000)
+	if b.Len() != 4 {
+		t.Errorf("TestLen Add failed.")
+	}
+	b.Remove(2)
+	b.Remove((3))
+	if b.Len() != 3 {
+		t.Errorf("TestLen Remove failed.")
+	}
+}
+
+func TestCCopy(t *testing.T) {
+	b := bitmap.NewC(3)
+	b.Add(-1)
+	b.Add(0)
+	b.Add(1)
+	b.Add(2)
+	b.Add(10000)
+	c := b.Copy()
+	if !c.Has(0) || !c.Has(1) || !c.Has(2) {
+		t.Errorf("TestCopy failed.")
+	}
+	b.Remove(2)
+	if !c.Has(2) {
+		t.Errorf("TestCopy failed.")
+	}
+}
+
+func TestCount(t *testing.T) {
+	b := bitmap.NewC(3)
+	b.Add(-1)
+	b.Add(0)
+	b.Add(1)
+	b.Add(2)
+	b.Add(2)
+	b.Add(2)
+	b.Add(1)
+	b.Add(10000)
+	if b.Count(-1) != 0 || b.Count(2) != 3 || b.Count(1) != 2 {
+		t.Errorf("TestCount Add failed.")
+	}
+	b.Remove(2)
+	if b.Count(2) != 2 {
+		t.Errorf("TestCount Remove failed.")
+	}
+	b.RemoveAll(1)
+	if b.Count(1) != 0 {
+		t.Errorf("TestCount RemoveAll failed.")
+	}
+}
+
+func BenchmarkCBitmap(b *testing.B) {
+	bm := bitmap.NewC(3)
+	const memory = 100000000
 	for i := 0; i < b.N; i++ {
 		bm.Add(i % memory)
 		bm.Has(i % memory)
